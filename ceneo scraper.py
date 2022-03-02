3a0d095860e42342f6a00256a6afffd0ca863277
Jakub-Oszczak <oszczak.jakub@gmail.com>
2022-03-02 23:22:30 +0100

    first commit

---
+++ ceneo scraper.py
@@ -0,0 +1,28 @@
+import requests
+import bs4
+from tkinter import *
+import selenium
+ 
+class App(Tk):
+    def __init__(self):
+        super().__init__()
+        self.geometry('500x500')
+        self.title('Ceneo scraper') 
+        self.product_var = StringVar(self)
+ 
+        self.create_widgets()
+ 
+    def make_request(self):
+        self.input = self.input_box.get()
+        r = requests.get('https://www.ceneo.pl/Smartfony;szukaj-iphone+11+64gb+czarny')
+ 
+    def create_widgets(self):
+        self.search_label = Label(self, text = "Search your prodcut:")
+        self.search_label.grid(row=0, column=0)
+        self.input_box = Entry(self)
+        self.input.grid(row=1, column=0)
+        self.search_button = Button(self, text='Search', command=self.make_request())
+ 
+if __name__ == '__main__':
+    app = App()
+    app.mainloop()
